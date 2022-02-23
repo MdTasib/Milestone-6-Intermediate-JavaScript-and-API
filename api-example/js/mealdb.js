af0839ef44963525f0788fa8e498db568483a017
MdTasib <ohidulalam442@gmail.com>
2022-02-23 22:53:53 +0600

    meal api

--- api-example/js/mealdb.js
+++ api-example/js/mealdb.js
@@ -15,7 +15,7 @@ function displayFood(foods) {
 		const div = document.createElement("div");
 		div.classList.add("col");
 		div.innerHTML = `
-          <div class="card">
+          <div onclick="loadDetail(${food.idMeal})" class="card">
 						<img src="${food.strMealThumb}" class="card-img-top" alt="..." />
 						<div class="card-body">
 							<h5 class="card-title">${food.strMeal}</h5>
@@ -25,4 +25,24 @@ function displayFood(foods) {
     `;
 		searchResult.appendChild(div);
 	});
+}
+
+function loadDetail(mealId) {
+	fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+		.then(response => response.json())
+		.then(data => displayDetail(data.meals[0]));
+}
+
+function displayDetail(meal) {
+	const mealDetail = document.getElementById("meal-details");
+	const div = document.createElement("div");
+	div.classList.add("card");
+	div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+    </div>
+  `;
+	mealDetail.appendChild(div);
 }
