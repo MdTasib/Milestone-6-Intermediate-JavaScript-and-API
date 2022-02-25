2adacbe4c8ed89179ce6f1f80a94ebf1ebf1ac30
MdTasib <ohidulalam442@gmail.com>
2022-02-25 09:02:28 +0600

    callback function

--- js-tricky/script.js
+++ js-tricky/script.js
@@ -4,3 +4,16 @@ console.log(typeof "tasib"); // string
 console.log(typeof true); // boolen
 console.log(typeof undefined); // undefined
  */
+
+// Callback function and pass different functions
+
+function goodMessage(name, good) {
+	good(name);
+}
+
+function goodMorning(name) {
+	console.log("Good Morning", name);
+}
+
+goodMessage("Tasib", goodMorning);
+goodMessage("Rafi", goodMorning);
